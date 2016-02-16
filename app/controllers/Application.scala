0d3d8eba139dd361fdac2524c7eee7affee98abc
rohitk <rohitkhatana.khatana@gmail.com>
2016-02-16 15:20:40 +0530

    working example for multiple aysnc calls

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,6 +7,8 @@ import javax.inject.Inject
 import play.api.libs.ws._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
+import scala.concurrent.Future
+
 class Application @Inject() (ws: WSClient) extends Controller {
 
   val request: WSRequest = ws.url("http://127.0.0.1:8000/category/?categoryId=6")
@@ -21,4 +23,16 @@ class Application @Inject() (ws: WSClient) extends Controller {
     }
   }
 
+  def show = Action.async {
+    val request2: WSRequest = ws.url("http://127.0.0.1:8000/category/")
+    val httpResponses = for {
+      result1 <- request2.get
+      result2 <- request2.get
+    } yield (result1.body + result2.body)
+
+    httpResponses.map {
+        Ok(_)
+    }
+
+  }
 }
